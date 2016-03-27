a097f73d3c79962f927e4ad72c6b44d35c66fe39
Sam <samuel1008@gmail.com>
2016-03-26 19:33:06 -0700

    redid the game, made it grid system for ease of use

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,38 @@
 $(function () {
 
-  var player = '<div id="player"></div>';
-  $("#map").append(player);
+  var player = 'x';
+  var player2 = '<div id="player2"></div>';
+  var move1 = 0;
+  $("#box1").append(player);
+  $("#map2").append(player2);
 
   $(document).keydown(function (e) {
-
+    console.log(1);
 
       var position = $("#player").position();
+      var position2 = $("#player2").position();
+
 
 
       switch (e.keyCode) {
         case 65:    // a - left
-          $("#player").css('left', position.left - 20 + 'px');
+          $("#player").css('left', position.left - 25 + 'px');
           break;
-        // case 87:    // w - top
-        //   $("#player").css('top', position.top -20 + 'px');
-        //   break;
+
         case 68:    // d - right
-          $("#player").css('left', position.left + 20 + 'px');
+          $("#player").css('left', position.left + 25 + 'px');
           break;
-        // case 83:    // s - down
-        //   $("#player").css('top', position.top +20 + 'px');
-        //   break;
+
+        case 39:    // -> - right
+          $("#player2").css('left', position2.left + 25 + 'px');
+          break;
+
+        case 37:    // <- - left
+          $("#player2").css('left', position2.left - 25 + 'px');
+          break;
 
       }
+      console.log(2);
 
   });
 
