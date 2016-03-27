2293a3f9814f17f418563a7f6c67e84bfbb4e3ba
Sam <samuel1008@gmail.com>
2016-03-27 15:46:37 -0700

    can move player left to right, alert player, and added a reset button

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,49 @@
-$(function () {
+$(document).ready(function() {
 
   var player = 'x';
-  var player2 = '<div id="player2"></div>';
-  var move1 = 0;
-  $("#box1").append(player);
-  $("#map2").append(player2);
+  var player2 = 'y';
+  var position1 = 1;
+  var position2 = 13;
 
-  $(document).keydown(function (e) {
-    console.log(1);
-
-      var position = $("#player").position();
-      var position2 = $("#player2").position();
+  $("#1").append(player);
+  $("#13").append(player2);
 
 
+  function gameplay() {
+    $(window).on('keypress', function handler(event) {
+      console.log(1);
+      if (event.which === 100) {
+        if ($("#" + position1).text !== "") {
+          $("#" + position1).empty();
+          position1++;
+          $("#" + position1).text(player);
+          console.log("after key press");
+        }
+        if (position1 === 12) {
+          alert(player + " " + "wins!!!");
 
-      switch (e.keyCode) {
-        case 65:    // a - left
-          $("#player").css('left', position.left - 25 + 'px');
-          break;
-
-        case 68:    // d - right
-          $("#player").css('left', position.left + 25 + 'px');
-          break;
-
-        case 39:    // -> - right
-          $("#player2").css('left', position2.left + 25 + 'px');
-          break;
-
-        case 37:    // <- - left
-          $("#player2").css('left', position2.left - 25 + 'px');
-          break;
-
+        }
       }
-      console.log(2);
+      if (event.which === 97) {
+        console.log("why ain this working!!!");
+        if ($("#" + position2).text !== "") {
+          $("#" + position2).empty();
+          position2++;
+          $("#" + position2).text(player2);
+          console.log("YEEEEE");
+        }
+      }
+      $("#reset").on('click', function() {
+        $("div.track div").empty();
+        $("#1").append(player);
+        $("#13").append(player2);
 
-  });
+      });
 
-
+      // else if ($("#24").text === player2){
+      //   alert(player2 + " " + "wins!!!");
+      // }
+    });
+  }
+  gameplay();
 });
